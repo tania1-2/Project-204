57d1e289aef0f29dc53b1ea0e29d4243b218715e
tania1-2 <98276777+tania1-2@users.noreply.github.com>
2023-12-26 12:57:51 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# PRO-C204-reference-code
+reference code
---
+++ server.py
@@ -0,0 +1,68 @@
+#--- Boilerplate code Start-------
+import socket
+from  threading import Thread
+
+SERVER = None
+PORT = None
+IP_ADDRESS = None
+
+CLIENTS = {}
+#--- Boilerplate code End-------
+
+
+# Boilerplate Code
+def acceptConnections():
+    global CLIENTS
+    global SERVER
+
+    while True:
+        player_socket, addr = SERVER.accept()
+
+        # --------- Student Activity Start
+        player_name = player_socket.recv(1024).decode().strip()
+    
+
+
+        if(len(CLIENTS.keys()) == 0):
+            CLIENTS[player_name] = {'player_type' : 'player1'}
+        else:
+            CLIENTS[player_name] = {'player_type' : 'player2'}
+
+
+        CLIENTS[player_name]["player_socket"] = player_socket
+        CLIENTS[player_name]["address"] = addr
+        CLIENTS[player_name]["player_name"] = player_name
+        CLIENTS[player_name]["turn"] = False
+
+        print(f"Connection established with {player_name} : {addr}")
+
+        # --------- Student Activity End
+
+
+
+
+
+# Boilerplate Code
+def setup():
+    print("\n")
+    print("\t\t\t\t\t\t*** LUDO LADDER ***")
+
+
+    global SERVER
+    global PORT
+    global IP_ADDRESS
+
+    IP_ADDRESS = '127.0.0.1'
+    PORT = 5000
+    SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    SERVER.bind((IP_ADDRESS, PORT))
+
+    SERVER.listen(10)
+
+    print("\t\t\t\tSERVER IS WAITING FOR INCOMMING CONNECTIONS...")
+    print("\n")
+
+    acceptConnections()
+
+
+setup()
